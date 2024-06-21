cf7d5b01532f5afd66662f03d25eac289a069525
Luke Roepe <35715588+ldroepe@users.noreply.github.com>
2024-06-20 21:15:34 -0400

    Update build.sh
    Co-authored-by: Balthazar Sicario <14964965+bzarbalthazar@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ build_dir=./build
 src_dir=./src
 
 test -d "$build_dir" || mkdir "$build_dir"
-cmake -S "$src_dir" -B "$build_dir"
+cmake -S "${src_dir}" -B "${build_dir}"
 cmake --build "$build_dir"
